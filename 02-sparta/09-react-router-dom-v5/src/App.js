f59e2dc091583c986742518f242cea2ebd3a1373
hyeonah <hyeonah.hello@gmail.com>
2022-04-11 18:47:34 +0900

    tutorial: Learn Switch component with creating NotFound page

--- 02-sparta/09-react-router-dom-v5/src/App.js
+++ 02-sparta/09-react-router-dom-v5/src/App.js
@@ -1,7 +1,8 @@
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import Cat from './Cat';
 import Dog from './Dog';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -17,9 +18,12 @@ function App() {
           <Link to="/dog">Dog으로 가기</Link>
         </li>
       </ul>
-      <Route path={'/'} component={Home} exact />
-      <Route path={'/cat'} component={Cat} />
-      <Route path={'/dog'} component={Dog} />
+      <Switch>
+        <Route path={'/'} component={Home} exact />
+        <Route path={'/cat'} component={Cat} />
+        <Route path={'/dog'} component={Dog} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
