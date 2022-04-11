854aa2dd38aa027a1e10a2d3dddd192c7609868a
hyeonah <hyeonah.hello@gmail.com>
2022-04-11 18:07:39 +0900

    tutorial project: Apply react-router-dom v5 to bucket list

---
+++ 02-sparta/10-bucket-list-with-detail-page/src/BucketList.js
@@ -0,0 +1,46 @@
+// 리액트 패키지를 불러옵니다.
+import React from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
+const BucketList = props => {
+  console.log(props);
+  console.log(props.list);
+  const my_lists = props.list;
+
+  const history = useHistory();
+
+  return (
+    <ListStyle>
+      {my_lists.map((list, index) => {
+        return (
+          <ItemStyle
+            className="list_item"
+            key={index}
+            onClick={() => {
+              history.push('/detail');
+            }}
+          >
+            {list}
+          </ItemStyle>
+        );
+      })}
+    </ListStyle>
+  );
+};
+
+const ListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+const ItemStyle = styled.div`
+  padding: 16px;
+  margin: 8px;
+  background-color: aliceblue;
+`;
+
+export default BucketList;
---
+++ 02-sparta/10-bucket-list-with-detail-page/src/Detail.js
@@ -0,0 +1,21 @@
+// 리액트 패키지를 불러옵니다.
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Detail = props => {
+  const history = useHistory();
+  return (
+    <>
+      <h1>상세 페이지입니다!</h1>
+      <button
+        onClick={() => {
+          history.push('/');
+        }}
+      >
+        돌아가기
+      </button>
+    </>
+  );
+};
+
+export default Detail;
