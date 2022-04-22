74fa007feddf31e8312189772d0ce9d8a1a6df40
hyeonah <hyeonah.hello@gmail.com>
2022-04-22 21:58:47 +0900

    doc: Update note and remove unnecessary action, action creator in bucket.js

--- 02-sparta/11-bucket-list-with-redux/src/redux/configStore.js
+++ 02-sparta/11-bucket-list-with-redux/src/redux/configStore.js
@@ -1,10 +1,15 @@
 // bucket module을 가지고 만든 Store
 
 import { createStore, combineReducers } from 'redux';
+// createStore: Store 생성
+// combineReducer: 여러개의 리듀서를 묶어주는 함수
 import bucket from './modules/bucket';
+// 리듀서 import하기
+// bucket.js에서 export default로 값을 내보내고 있다
 
 // Root Reducer
-// 나중에 리듀서를 여러개 만들게 되면 여기에 하나씩 추가
+// 나중에 리듀서를 여러개 만들게 되면 combineReducer()함수의 파라미터로 하나씩 추가
+// 예) combineReducers({ bucket, newReducer... });
 const rootReducer = combineReducers({ bucket });
 // const rootReducer = combineReducers({ bucket: bucket });
 
--- 02-sparta/11-bucket-list-with-redux/src/redux/modules/bucket.js
+++ 02-sparta/11-bucket-list-with-redux/src/redux/modules/bucket.js
@@ -1,22 +1,16 @@
 // bucket module
 
 // Initial State
-// 리덕스에서 저장하고 있는 상태값(=데이터)
+// 리덕스에서 저장하고 있는 초기 상태(=데이터)
 const initialState = {
   list: ['영화관 가기', '매일 책읽기', '수영 배우기'],
 };
 
-// Actions
-const LOAD = 'bucket/LOAD';
+// Action Type
 const CREATE = 'bucket/CREATE';
 
 // Action Creators
 // 액션 생성 함수로 액션을 만들기 위해 사용하는 함수
-export function loadBucket(bucket) {
-  // Action
-  return { type: LOAD, bucket };
-}
-
 export function createBucket(bucket) {
   console.log('액션생성함수 실행! state 업데이트 요청!');
   return { type: CREATE, bucket: bucket };
@@ -25,6 +19,7 @@ export function createBucket(bucket) {
 // Reducer
 // store에 들어가 있는 데이터를 변경하는 곳
 // 1) State와 2) ActionCreator에서 만든 액션 객체로 현재 상태를 업데이트 해준다
+// reducer 함수가 호출될때 action이 들어오지 않을경우 디폴트 파라미터로 설정된 빈객체가 들어간다
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case 'bucket/LOAD':
